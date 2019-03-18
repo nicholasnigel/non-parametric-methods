a98aa5556d42df2f681ecc9fae5659fdd285d146
Nigel Nicholas <>
2019-03-18 21:08:05 +0800

    2.3.2 finished

--- poly_regular.py
+++ poly_regular.py
@@ -14,15 +14,37 @@ poly = PolynomialFeatures(degree=5)
 X_train_poly = poly.fit_transform(X_train)
 X_test_poly = poly.transform(X_test)
 
+pr_model = LinearRegression()
+pr_model.fit(X_train_poly,y_train)
+
+print('intercept_: ',pr_model.intercept_)
+print('coef_: ', pr_model.coef_)
+
+print('Linear regression (order 5) score is: ',pr_model.score(X_test_poly,y_test))
 # some code here
 
 xx = np.linspace(0, 26, 100)
 xx_poly = poly.transform(xx.reshape(xx.shape[0], 1))
 yy_poly = pr_model.predict(xx_poly)
 
-# some code here
+# some code here (plotting)
 
-ridge_model = Ridge(alpha=4, normalize=False)
+plt.plot(xx ,yy_poly, 'b.')
+plt.plot(X_test,y_test, 'r.')
+plt.show()
+
+# ====================================  Regression through ridge    ====================================================
+
+ridge_model = Ridge(alpha=1, normalize=False)   # fix to alpha = 1
 ridge_model.fit(X_train_poly, y_train)
 
 # some code here
+print('ridge model intercept: ', ridge_model.intercept_)
+print('ridge model coefficient: ', ridge_model.coef_)
+
+print('ridge regression scors is: ', ridge_model.score(X_test_poly, y_test))
+
+yy_ridge = ridge_model.predict(xx_poly)
+plt.plot(xx,yy_ridge  ,'r')
+plt.plot(X_test,y_test, 'b')
+plt.show()
